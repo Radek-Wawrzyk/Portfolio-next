da945610f65146d0a0b8abbe00600faa97f5368e
Radek <radekwawrzyk@gmail.com>
2021-05-13 16:17:53 +0200

    Footer - Create MainFooter component with some small CSS changes

---
+++ src/components/Main/MainFooter/MainFooter.vue
@@ -0,0 +1,193 @@
+<template>
+  <footer class="main-footer">
+    <header class="main-footer__header">
+      <div class="main-footer__header-content container">
+        <h3 class="main-footer__header-name">
+          Contact me
+        </h3>
+
+        <base-logo class="main-footer__header-logo" />
+      </div>
+    </header>
+
+    <div class="main-footer-content">
+      <div class="container main-footer-content__wrapper">
+        <div class="main-footer-content__info">
+          <ul 
+            class="main-footer-content__block"
+            v-for="(block, index) in footerInfo"
+            :key="index"
+          >
+            <ul class="main-footer-content__block-list">
+              <li
+                class="main-footer-content__block-item"
+                v-for="infoItem in block.items"
+                :key="infoItem.name"
+              >
+                <a
+                  v-if="infoItem.link"
+                  :href="infoItem.link"
+                  :aria-label="infoItem.name"
+                  :title="infoItem.name"
+                  class="main-footer-content__block-link"
+                >
+                  {{ infoItem.name }}
+                </a>
+                
+                <p class="main-footer-content__block-text" v-else>
+                  {{ infoItem.name }}
+                </p>
+              </li>
+            </ul>
+          </ul>
+        </div>
+
+        <ul class="main-footer-content__navigation">
+          <li 
+            class="main-footer-content__navigation-item"
+            v-for="menuItem in footerMenu"
+            :key="menuItem.name"
+          >
+            <router-link
+              :to="menuItem.link"
+              :aria-label="menuItem.name"
+              :title="menuItem.name"
+              class="main-footer-content__navigation-link"
+            >
+              {{ menuItem.name }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="main-footer-info">
+      <div class="main-footer-info__wrapper container">
+        <p class="main-footer-info__copyright-text">
+          © 2021 Radek Wawrzyk - designed with love by jakubkowalski.dev  
+        </p>
+
+        <ul class="main-footer-info__socials">
+          <li 
+            class="main-footer-info__socials-item"
+            v-for="menuItem in footerSocials"
+            :key="menuItem.name"
+          >
+            <a
+              :href="menuItem.link"
+              :aria-label="menuItem.name"
+              :title="menuItem.name"
+              class="main-footer-info__socials-link"
+              v-html="menuItem.icon"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import BaseLogo from '@/components/Base/BaseLogo/BaseLogo.vue';
+import { defineComponent, reactive } from 'vue';
+
+export default defineComponent({
+  components: { BaseLogo },
+  name: 'MainFooter',
+  setup() {
+    const footerMenu = reactive([
+      {
+        name: 'Home',
+        link: '/',
+      },
+      {
+        name: 'About',
+        link: '/about',
+      },
+      {
+        name: 'Services',
+        link: '/services',
+      },
+      {
+        name: 'Works',
+        link: '/works',
+      },
+      {
+        name: 'Contact',
+        link: '/contact',
+      },
+      {
+        name: 'Privacy Policy',
+        link: '/privacy-policy',
+      },
+    ]);
+    
+    const footerInfo = reactive([
+      {
+        items: [
+          {
+            name: 'Radosław Wawrzyk',
+            link: null,
+          },
+          {
+            name: 'radekwawrzyk@gmail.com',
+            link: 'mailto:radekwawrzyk@gmail.com',
+          },
+          {
+            name: '+48 516 649 611',
+            link: 'tel:+48516649611',
+          },
+        ],
+      },
+      // {
+      //   items: [
+      //     {
+      //       name: 'Front-End Engineer',
+      //       link: null,
+      //     },
+      //     {
+      //       name: 'Aberystwyth, United Kingdom',
+      //       link: null,
+      //     },
+      //     {
+      //       name: 'Gdynia, Poland',
+      //       link: null,
+      //     },
+      //   ],
+      // },
+    ]);
+
+    const footerSocials = reactive([
+      {
+        name: 'Facebook',
+        icon: '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 167.657 167.657" style="enable-background:new 0 0 167.657 167.657;" xml:space="preserve"> <g> <path style="fill:#ffffff;" d="M83.829,0.349C37.532,0.349,0,37.881,0,84.178c0,41.523,30.222,75.911,69.848,82.57v-65.081H49.626 v-23.42h20.222V60.978c0-20.037,12.238-30.956,30.115-30.956c8.562,0,15.92,0.638,18.056,0.919v20.944l-12.399,0.006 c-9.72,0-11.594,4.618-11.594,11.397v14.947h23.193l-3.025,23.42H94.026v65.653c41.476-5.048,73.631-40.312,73.631-83.154 C167.657,37.881,130.125,0.349,83.829,0.349z"/> </g></svg>',
+        link: 'https://www.facebook.com/radek.wawrzyk/'
+      },
+      {
+        name: 'Instagram',
+        icon: '<svg xmlns="http://www.w3.org/2000/svg" height="511pt" viewBox="0 0 511 511.9" width="511pt"><path d="m510.949219 150.5c-1.199219-27.199219-5.597657-45.898438-11.898438-62.101562-6.5-17.199219-16.5-32.597657-29.601562-45.398438-12.800781-13-28.300781-23.101562-45.300781-29.5-16.296876-6.300781-34.898438-10.699219-62.097657-11.898438-27.402343-1.300781-36.101562-1.601562-105.601562-1.601562s-78.199219.300781-105.5 1.5c-27.199219 1.199219-45.898438 5.601562-62.097657 11.898438-17.203124 6.5-32.601562 16.5-45.402343 29.601562-13 12.800781-23.097657 28.300781-29.5 45.300781-6.300781 16.300781-10.699219 34.898438-11.898438 62.097657-1.300781 27.402343-1.601562 36.101562-1.601562 105.601562s.300781 78.199219 1.5 105.5c1.199219 27.199219 5.601562 45.898438 11.902343 62.101562 6.5 17.199219 16.597657 32.597657 29.597657 45.398438 12.800781 13 28.300781 23.101562 45.300781 29.5 16.300781 6.300781 34.898438 10.699219 62.101562 11.898438 27.296876 1.203124 36 1.5 105.5 1.5s78.199219-.296876 105.5-1.5c27.199219-1.199219 45.898438-5.597657 62.097657-11.898438 34.402343-13.300781 61.601562-40.5 74.902343-74.898438 6.296876-16.300781 10.699219-34.902343 11.898438-62.101562 1.199219-27.300781 1.5-36 1.5-105.5s-.101562-78.199219-1.300781-105.5zm-46.097657 209c-1.101562 25-5.300781 38.5-8.800781 47.5-8.601562 22.300781-26.300781 40-48.601562 48.601562-9 3.5-22.597657 7.699219-47.5 8.796876-27 1.203124-35.097657 1.5-103.398438 1.5s-76.5-.296876-103.402343-1.5c-25-1.097657-38.5-5.296876-47.5-8.796876-11.097657-4.101562-21.199219-10.601562-29.398438-19.101562-8.5-8.300781-15-18.300781-19.101562-29.398438-3.5-9-7.699219-22.601562-8.796876-47.5-1.203124-27-1.5-35.101562-1.5-103.402343s.296876-76.5 1.5-103.398438c1.097657-25 5.296876-38.5 8.796876-47.5 4.101562-11.101562 10.601562-21.199219 19.203124-29.402343 8.296876-8.5 18.296876-15 29.398438-19.097657 9-3.5 22.601562-7.699219 47.5-8.800781 27-1.199219 35.101562-1.5 103.398438-1.5 68.402343 0 76.5.300781 103.402343 1.5 25 1.101562 38.5 5.300781 47.5 8.800781 11.097657 4.097657 21.199219 10.597657 29.398438 19.097657 8.5 8.300781 15 18.300781 19.101562 29.402343 3.5 9 7.699219 22.597657 8.800781 47.5 1.199219 27 1.5 35.097657 1.5 103.398438s-.300781 76.300781-1.5 103.300781zm0 0"/><path d="m256.449219 124.5c-72.597657 0-131.5 58.898438-131.5 131.5s58.902343 131.5 131.5 131.5c72.601562 0 131.5-58.898438 131.5-131.5s-58.898438-131.5-131.5-131.5zm0 216.800781c-47.097657 0-85.300781-38.199219-85.300781-85.300781s38.203124-85.300781 85.300781-85.300781c47.101562 0 85.300781 38.199219 85.300781 85.300781s-38.199219 85.300781-85.300781 85.300781zm0 0"/><path d="m423.851562 119.300781c0 16.953125-13.746093 30.699219-30.703124 30.699219-16.953126 0-30.699219-13.746094-30.699219-30.699219 0-16.957031 13.746093-30.699219 30.699219-30.699219 16.957031 0 30.703124 13.742188 30.703124 30.699219zm0 0"/></svg>',
+        link: 'https://www.instagram.com/radziolada/',
+      },
+      {
+        name: 'LinkedIn',
+        icon: '<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-linkedin fa-w-14"><path fill="currentColor" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path></svg>',
+        link: 'https://www.linkedin.com/in/radek-wawrzyk-211948158/',
+      },
+      {
+        name: 'Github',
+        icon: '<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="svg-inline--fa fa-github fa-w-16"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>',
+        link: 'https://github.com/Radek-Wawrzyk',
+      },
+    ]);
+
+    return {
+      footerMenu,
+      footerInfo,
+      footerSocials,
+    };
+  },
+});
+
+</script>
+
+<style lang="scss" src="./MainFooter.scss" />
